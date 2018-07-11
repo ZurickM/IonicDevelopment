ea61666d005591e6444b264084b69c967d951248
ZurickM <40615777+ZurickM@users.noreply.github.com>
2018-07-11 11:00:33 -0700

    First Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SecondPage } from '../second/second';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {}
+nextPage(){
+  this.navCtrl.push(SecondPage);
+  }
+
+  }
