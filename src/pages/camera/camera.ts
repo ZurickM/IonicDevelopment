055c7b967ee9448bc57758eee8350439c9caa84d
ZurickM <40615777+ZurickM@users.noreply.github.com>
2018-07-12 03:32:49 -0700

    camera3

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the CameraPage page.
@@ -25,3 +26,5 @@ export class CameraPage {
   }
 
 }
+
+
