a5fe748cf4fc7006ec553654d4cdcdddaad75b4f
ZurickM <40615777+ZurickM@users.noreply.github.com>
2018-07-13 21:58:05 -0700

    Camera

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -17,15 +17,14 @@ formPage;
 InsolePage: any;
 myphoto: any;
 constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
-this.pushPage = HomePage;
-this.formPage = HomePage;
-this.InsolePage= InsolePage;
-    
-
-console.log(navParams.get('val'));
+  this.pushPage = HomePage;
+  this.formPage = HomePage;
+  this.InsolePage= InsolePage;
+  
+  console.log(navParams.get('val'));
 
   }
-
+  
   nextPage(){
     this.navCtrl.push(HomePage);
     }
@@ -36,23 +35,23 @@ console.log(navParams.get('val'));
       }
 
 
-takePhoto(){
-  const options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
+  takePhoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
   
   this.camera.getPicture(options).then((imageData) => {
    // imageData is either a base64 encoded string or a file URI
    // If it's base64 (DATA_URL):
-   this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    this.myphoto = 'data:image/jpeg;base64,' + imageData;
   }, (err) => {
    // Handle error
   });
 
-}
+  }
 
 
 }
