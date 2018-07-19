4c7878d5b74aca5f67b66d6a197016bbed2944a6
ZurickM <40615777+ZurickM@users.noreply.github.com>
2018-07-19 14:31:12 -0700

    buttons

--- src/pages/insole/insole.ts
+++ src/pages/insole/insole.ts
@@ -4,20 +4,24 @@ import { HomePage } from '../home/home';
 
 import { ProinfoPage } from '../proinfo/proinfo';
 import { UnderproPage } from '../underpro/underpro';
+import { analyzeAndValidateNgModules } from '../../../node_modules/@angular/compiler';
 /**
  * Generated class for the InsolePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-insole',
   templateUrl: 'insole.html',
 })
 export class InsolePage {
-Home;
+  Home;
+  pageone = false;
+  pagetwo = false;
+  pagethree = false;
+  selected = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.Home = HomePage;
   }
@@ -35,4 +39,44 @@ Home;
     console.log('ionViewDidLoad InsolePage');
   }
 
-}
+  ifButton(letter){
+    let position = this.selected.indexOf(letter)
+    if (position == -1){
+      this.selected.push(letter);
+    }else{
+      this.selected.splice(position, 1);
+    }
+    console.log(this.selected)
+    this.analyze();
+  }
+
+  analyze(){
+    if (this.selected.indexOf('C') != -1 && this.selected.indexOf('B') != -1 && this.selected.length == 2){
+      this.pageone = true;
+      this.pagetwo = false;
+      this.pagethree = false;
+   
+    }
+    if(this.selected.indexOf('C') != -1 && this.selected.indexOf('A') != -1 && this.selected.length == 2){
+      this.pageone = false;
+      this.pagetwo = true;
+      this.pagethree = false;
+    }
+    
+
+
+    if(this.selected.indexOf('D') != -1 && this.selected.indexOf('A') != -1 && this.selected.length == 2){
+      this.pageone = false;
+      this.pagetwo = true;
+      this.pagethree = false;
+    
+    } if(this.selected.indexOf('E') != -1 && this.selected.indexOf('A') != -1 && this.selected.length == 2){
+          this.pageone = false;
+          this.pagetwo = false;
+          this.pagethree = true;
+    }
+    
+         
+      
+    }
+  }
